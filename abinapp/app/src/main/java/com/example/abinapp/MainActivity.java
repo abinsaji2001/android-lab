cecfea9164df14e6aa48f24afcf5530ec5917774
Your Name <you@example.com>
2024-09-07 12:19:24 +0530

    first commit

---
+++ abinapp/app/src/main/java/com/example/abinapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.abinapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edt1,edt2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void login(View view) {
+        Toast.makeText(this, "login successful", Toast.LENGTH_SHORT).show();
+    }
+    public void clear(View view) {
+        edt2.setText(" ");
+        edt2.setText(" ");
+    }
+}
